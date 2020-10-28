e8e64a9e8c03d9346a1dcc015f29fbe260de88a8
Aleksandr Smirnov <teppopuctka2@gmail.com>
2020-10-28 11:56:58 +0000

    repl.it nepieciešamās izmaiņas

---
+++ strings.py
@@ -0,0 +1,30 @@
+print('Hello World!')
+
+va1= 'Sveika '
+print(va1)
+
+va2 ='Pasaule'
+print(va1,va2)
+
+print(len(va1))
+
+print("val[2:4]:", va1[::-1])
+
+print(va1*2)
+
+va2 = 'Sashka'
+print(va1+va2)
+#pirma metode
+print("Mes te - %s - kaut ko ieleksim"
+%'kaut kas', end='\n\n')
+
+#otra metode
+print("Teksta ievetosana ar .format metodi: {}". format ('Hey, tev sanaca'))
+
+print("{1} {0}".format('Juuhhhuu,','sacanaca!!!!!'))
+print("{0} {1}".format('Juuhhhuu,','sacanaca!!!!!'))
+
+#3 metode
+
+vards= "saule"
+print(f"Ara spid {vards}")
